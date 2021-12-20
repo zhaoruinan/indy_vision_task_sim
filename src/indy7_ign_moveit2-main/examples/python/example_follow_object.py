222c2504e8e7a9156db6ad92b0d3a6b596394991
LEESHOHYUN <jjjenny@hanyang.ac.kr>
2021-12-21 00:30:16 +0900

    if lucky, pick

--- src/indy7_ign_moveit2-main/examples/python/example_follow_object.py
+++ src/indy7_ign_moveit2-main/examples/python/example_follow_object.py
@@ -5,7 +5,6 @@ from moveit2 import MoveIt2Interface
 from rclpy.node import Node
 import rclpy
 import time
-
 class ObjectFollower(Node):
 
     def __init__(self):
@@ -25,80 +24,87 @@ class ObjectFollower(Node):
         self.executor_.add_node(self.moveit2_)
         self.executor_.spin()
 
-    # def object_pose_callback(self, pose_msg):
-    #     if self.previous_object_pose_ != pose_msg:
-    #         self.throw(pose_msg.position)
-    #         self.destroy_subscription(self._object_pose_sub)
-    #         self.previous_object_pose_ = pose_msg
-    #         rclpy.shutdown()
-    #         exit(0)
+    def object_pose_callback(self, pose_msg):
+            self.throw(pose_msg.position)
+            self.destroy_subscription(self._object_pose_sub)
+            rclpy.shutdown()
+            exit(0)
 
-    # def throw(self, object_position):
-    #     # Open gripper
+    def throw(self, object_position):
+        a=1
+        if (a==1) :
+        # Open gripper
+            self.moveit2_.gripper_open()
+            self.moveit2_.wait_until_executed()
+            a=3
+        # if (a==2):
+        #     # Move above object
+        #     position = [object_position.x,
+        #                 object_position.y+0.05, object_position.z + 0.1]
+        #     quaternion = [1.0, 0.0, 0.0, 0.0]
+        #     self.moveit2_.set_pose_goal(position, quaternion)
+        #     self.moveit2_.plan_kinematic_path()
+        #     self.moveit2_.execute()
+        #     self.moveit2_.wait_until_executed()
+        #     a=3
+        if(a==3):
+            # Move above object
+            position = [object_position.x,
+                        object_position.y+0.03, object_position.z + 0.05]
+            quaternion = [1.0, 0.0, 0.0, 0.0]
+            self.moveit2_.set_pose_goal(position, quaternion)
+            self.moveit2_.plan_kinematic_path()
+            self.moveit2_.execute()
+            self.moveit2_.wait_until_executed()
+            a=4
+        if(a==4):
+            # Close gripper
+            self.moveit2_.gripper_close(width=0.05, speed=0.2, force=20.0)
+            self.moveit2_.wait_until_executed()
+            a=0
+
+
+
+
+        # self.moveit2_.set_max_velocity(0.5)
+        # self.moveit2_.set_max_acceleration(0.5)
+
+
+
+
+        # if Yolo Object Detection executed and if voice recognition successed
+        # only chase the ordered object
+        # if()
+
+
+
+
+    # def object_pose_callback(self, pose_msg):
+    #     # Plan trajectory only if object was moved
+    #     if self.previous_object_pose_ != pose_msg:
+    #         self.GripperOpen()
+
+    #         self.moveit2_.set_pose_goal([pose_msg.position.x,
+    #                                         pose_msg.position.y+0.03,
+    #                                         pose_msg.position.z+0.05],
+    #                                     [pose_msg.orientation.x,
+    #                                         pose_msg.orientation.y,
+    #                                         pose_msg.orientation.z,
+    #                                         pose_msg.orientation.w])
+    #         self.moveit2_.plan_kinematic_path()
+    #         self.moveit2_.execute()
+
+    #         # Update for next callback
+    #         self.previous_object_pose_ = pose_msg
+
+    # def GripperOpen(self):
     #     self.moveit2_.gripper_open()
     #     self.moveit2_.wait_until_executed()
 
-    #     # Move above object
-    #     position = [object_position.x,
-    #                 object_position.y+0.02, object_position.z + 0.1]
-    #     quaternion = [1.0, 0.0, 0.0, 0.0]
-    #     self.moveit2_.set_pose_goal(position, quaternion)
-    #     self.moveit2_.plan_kinematic_path()
-    #     self.moveit2_.execute()
+    # def GripperClose(self):
+
+    #     self.moveit2_.gripper_close(width=0.02, speed=0.2, force=20.0)
     #     self.moveit2_.wait_until_executed()
-
-    #     self.moveit2_.set_max_velocity(0.5)
-    #     self.moveit2_.set_max_acceleration(0.5)
-
-    #     # Move to grasp position
-    #     position = [object_position.x,
-    #                 object_position.y+0.02, object_position.z]
-    #     quaternion = [1.0, 0.0, 0.0, 0.0]
-    #     self.moveit2_.set_pose_goal(position, quaternion)
-    #     self.moveit2_.plan_kinematic_path()
-    #     self.moveit2_.execute()
-    #     self.moveit2_.wait_until_executed()
-
-    #     # Close gripper
-    #     self.moveit2_.gripper_close(width=0.05, speed=0.2, force=20.0)
-    #     self.moveit2_.wait_until_executed()
-
-    #     # Move above object again
-    #     position = [object_position.x,
-    #                 object_position.y+0.02, object_position.z + 0.1]
-    #     quaternion = [1.0, 0.0, 0.0, 0.0]
-    #     self.moveit2_.set_pose_goal(position, quaternion)
-    #     self.moveit2_.plan_kinematic_path()
-    #     self.moveit2_.execute()
-    #     self.moveit2_.wait_until_executed()
-
-
-
-    def object_pose_callback(self, pose_msg):
-        # Plan trajectory only if object was moved
-        if self.previous_object_pose_ != pose_msg:
-            self.GripperOpen()
-            self.moveit2_.set_pose_goal([pose_msg.position.x,
-                                            pose_msg.position.y+0.03,
-                                            pose_msg.position.z+0.05],
-                                        [pose_msg.orientation.x,
-                                            pose_msg.orientation.y,
-                                            pose_msg.orientation.z,
-                                            pose_msg.orientation.w])
-            self.moveit2_.plan_kinematic_path()
-            self.moveit2_.execute()
-
-            # Update for next callback
-            self.previous_object_pose_ = pose_msg
-
-    def GripperOpen(self):
-        self.moveit2_.gripper_open()
-        self.moveit2_.wait_until_executed()
-
-    def GripperClose(self):
-
-        self.moveit2_.gripper_close(width=0.02, speed=0.2, force=20.0)
-        self.moveit2_.wait_until_executed()
 
 
 
